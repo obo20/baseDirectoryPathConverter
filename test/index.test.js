01d52366188a1accd4c9f4e90e9a55604c0c3bf6
Matt Ober <>
2019-05-24 14:40:54 -0500

    added package.json updates and test updates
    added package.json updates and test updates

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,10 @@
-const convertFilePathToBaseDirectoryPath = require('./../index.js');
+const baseDirectoryConverter = require('./../index.js');
 
 describe('convertToRootDirectoryPath testing ', () => {
     test('ending / is removed', () => {
-        expect(convertFilePathToBaseDirectoryPath('example/', 'example/')).toBe('example');
+        expect(baseDirectoryConverter('example/', 'example/')).toBe('example');
     });
     test('relative path is changed', () => {
-        expect(convertFilePathToBaseDirectoryPath('biggerParent/parentFolder/sourceFolder', 'biggerParent/parentFolder/sourceFolder/example/test.file')).toBe('sourceFolder/example/test.file');
+        expect(baseDirectoryConverter('parentDirectory/childDirectory/sourceDirectory', 'parentDirectory/childDirectory/sourceDirectory/example/test.file')).toBe('sourceDirectory/example/test.file');
     });
 });
